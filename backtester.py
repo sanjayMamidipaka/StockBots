622bf8d1d16df2b1c539099e573df31d9f265045
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2020-05-04 18:27:00 -0400

    added visual buy and sell points

--- backtester.py
+++ backtester.py
@@ -2,37 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-bbands = pd.read_csv('bbands.csv', index_col='time', parse_dates=True)
-average = pd.read_csv('total.csv', index_col='timestamp', parse_dates=True)
-
-average = average.iloc[::-1]
-bbands = bbands.iloc[::-1]
-
-bbands.drop('Real Middle Band', axis=1, inplace=True)
-bbands['Average'] = average['close']
-
 
 class Backtester(object):
 
 
-    def __init__(self, init):
+    def __init__(self, init, indicator):
         self.first = init
         self.initialInvestment = init
         self.buys = 0
+        self.borrows = 0
+        self.indicator = indicator
 
     def sell(self, shares, price, i):
         if self.buys > 0:
             self.initialInvestment += (shares * price)
-            print(bbands.index[i], bbands['Average'][i], 'SELL', self.initialInvestment)
-            self.buys -= 1
-            return self.initialInvestment
+            print(self.indicator.index[i], self.indicator['Average'][i], 'SHORT')
+        return self.initialInvestment
 
     def buy(self, shares, price, i):
         if (not (price*shares > self.initialInvestment)):
             self.buys += 1
             self.initialInvestment -= (shares * price)
-            print(bbands.index[i], bbands['Average'][i], 'BUY', self.initialInvestment)
-            return self.initialInvestment
+            print(self.indicator.index[i], self.indicator['Average'][i], 'BUY')
+        return self.initialInvestment
 
     def get_returns(self):
         return 'Profit: ' + str(self.initialInvestment - self.first)
--- clean.py
+++ clean.py
@@ -4,8 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 initialInvestment = 10000.0
-b = backtester.Backtester(initialInvestment) 
-def bbands():
+sellx = []
+selly = []
+
+buyx = []
+buyy = []
+
+def bbands(initialInvestment):
     bbands = pd.read_csv('bbands.csv', index_col='time', parse_dates=True)
     average = pd.read_csv('total.csv', index_col='timestamp', parse_dates=True)
 
@@ -16,33 +21,52 @@ def bbands():
     bbands.drop('Real Middle Band', axis=1, inplace=True)
 
     bbands['Average'] = average['close']
+    b = backtester.Backtester(initialInvestment, bbands)
     for i in range(len(bbands.index)): 
         if (bbands['Real Upper Band'][i] - bbands['Average'][i] < 2): #sell
-            b.sell(50, float(bbands['Average'][i]), i)
+            b.sell(10, float(bbands['Average'][i]), i)
+            sellx.append(bbands.index[i])
+            selly.append(bbands['Average'][i])
 
         if (bbands['Average'][i] - bbands['Real Lower Band'][i] < 2): #buy
-            b.buy(50, float(bbands['Average'][i]), i)
+            b.buy(10, float(bbands['Average'][i]), i)
+            buyx.append(bbands.index[i])
+            buyy.append(bbands['Average'][i])
 
     print(b.get_returns())
     bbands.plot()
 
 
-# def vwap():
-#     vwap = pd.read_csv('vwap.csv', index_col='time', parse_dates=True)
-#     intraday = pd.read_csv('intraday.csv', index_col='timestamp', parse_dates=True)
+def vwap(initialInvestment):
+    vwap = pd.read_csv('vwap.csv', index_col='time', parse_dates=True)
+    intraday = pd.read_csv('intraday.csv', index_col='timestamp', parse_dates=True)
 
-#     vwap = vwap.iloc[::-1]
-#     intraday = intraday.iloc[::-1]
+    vwap = vwap.iloc[::-1]
+    intraday = intraday.iloc[::-1]
 
-#     vwap['Average'] = intraday['close']
-#     vwap.plot()
-#     # for i in range(len(bbands.index)): 
-#     #     if (vwap['vwap'][i] - vwap['Average'][i] < 0.1 && ): #sell
-#     #         initialInvestment = b.sell(51, float(vwap['Average'][i]))
+    vwap = vwap[-1000:]
+    intraday = intraday[-1000:]
 
-#     #     elif (bbands['Average'][i] - bbands['Real Lower Band'][i] < 2): #buy
-#     #         initialInvestment = b.buy(51, float(bbands['Average'][i]))
+    vwap['Average'] = intraday['close']
+    b = backtester.Backtester(initialInvestment, vwap)
+    vwap.plot(figsize=(20,4))
+    for i in range(0,len(vwap.index),3): 
+        if (vwap['vwap'][i] - vwap['Average'][i] < 0.1): #sell
+            initialInvestment = b.sell(10, float(vwap['Average'][i]), i)
+            sellx.append(vwap.index[i])
+            selly.append(vwap['Average'][i])
+
+        elif (vwap['Average'][i] - vwap['vwap'][i] < 0.1): #buy
+            initialInvestment = b.buy(10, float(vwap['Average'][i]), i)
+            buyx.append(vwap.index[i])
+            buyy.append(vwap['Average'][i])
+
+    print(b.get_returns())
+
     
 
-bbands()
+vwap(initialInvestment)
+plt.scatter(sellx, selly,c='red', label='sell')
+plt.scatter(buyx, buyy,c='green', label='buy')
+plt.legend()
 plt.show()
