428cd31c210a3e4355be86a7da769d9da38c476c
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2020-05-05 14:42:38 -0400

    streamlined files and input process

--- clean.py
+++ clean.py
@@ -2,8 +2,9 @@ import backtester
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
+import requests, csv
 
-initialInvestment = 110000.0
+initialInvestment = 1000.0
 sellx = []
 selly = []
 
@@ -11,39 +12,56 @@ buyx = []
 buyy = []
 
 def bbands(initialInvestment):
-    bbands = pd.read_csv('bbands.csv', index_col='time', parse_dates=True)
-    average = pd.read_csv('total.csv', index_col='timestamp', parse_dates=True)
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY'
+    r = '{}&symbol={}&interval={}&apikey={}&datatype={}&outputsize={}'.format(url, 'MSFT', '1min','9YM6MWUWHMZN05MB', 'csv','full')
+
+    average = pd.read_csv(r, index_col='timestamp', parse_dates=True)
+
+    url = 'https://www.alphavantage.co/query?function=BBANDS&series_type=close&nbdevup=3&nbdevdn=3'
+    r = '{}&symbol={}&interval={}&time_period={}&apikey={}&datatype={}'.format(url, 'MSFT', 'daily','5','9YM6MWUWHMZN05MB', 'csv','full')
+
+    bbands = pd.read_csv(r, index_col='time', parse_dates=True)
 
     average = average.iloc[::-1]
     bbands = bbands.iloc[::-1]
 
+    average = average[-1000:]
+    bbands = bbands[-1000:]
 
     bbands.drop('Real Middle Band', axis=1, inplace=True)
 
     bbands['Average'] = average['close']
     b = backtester.Backtester(initialInvestment, bbands)
-    for i in range(len(bbands.index)): 
+    for i in range(0,len(bbands.index)): 
         if (bbands['Real Upper Band'][i] - bbands['Average'][i] < 2): #sell
-            boolean = b.sell(57, float(bbands['Average'][i]), i)
+            boolean = b.sell(1, float(bbands['Average'][i]), i)
             
             if boolean:
                 sellx.append(bbands.index[i])
                 selly.append(bbands['Average'][i])
 
         if (bbands['Average'][i] - bbands['Real Lower Band'][i] < 2): #buy
-            boolean = b.buy(57, float(bbands['Average'][i]), i)
+            boolean = b.buy(1, float(bbands['Average'][i]), i)
 
             if boolean:
                 buyx.append(bbands.index[i])
                 buyy.append(bbands['Average'][i])
 
     print(b.get_returns())
-    bbands.plot(figsize=(20,8))
+    bbands.plot(figsize=(20,12))
 
 
 def vwap(initialInvestment):
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY'
+    r = '{}&symbol={}&interval={}&apikey={}&datatype={}&outputsize={}'.format(url, 'MSFT', '1min','9YM6MWUWHMZN05MB', 'csv','full')
+
+    intraday = pd.read_csv(r, index_col='timestamp', parse_dates=True)
+
+    url = 'https://www.alphavantage.co/query?function=VWAP'
+    r = '{}&symbol={}&interval={}&apikey={}&datatype={}&outputsize={}'.format(url, 'MSFT', '1min','9YM6MWUWHMZN05MB', 'csv','full')
+
     vwap = pd.read_csv('vwap.csv', index_col='time', parse_dates=True)
-    intraday = pd.read_csv('intraday.csv', index_col='timestamp', parse_dates=True)
+
 
     vwap = vwap.iloc[::-1]
     intraday = intraday.iloc[::-1]
@@ -51,16 +69,16 @@ def vwap(initialInvestment):
 
     vwap['Average'] = intraday['close']
     b = backtester.Backtester(initialInvestment, vwap)
-    vwap.plot(figsize=(20,4))
-    for i in range(len(vwap.index)): 
-        if (vwap['vwap'][i] - vwap['Average'][i] < 0.1): #sell
+    vwap.plot(figsize=(20,12))
+    for i in range(0,len(vwap.index)): 
+        if (vwap['vwap'][i] - vwap['Average'][i] < 2): #sell
             boolean = initialInvestment = b.sell(2, float(vwap['Average'][i]), i) # returns true if atrade was actually executed
 
             if boolean: # if a trade was executed, plot it
                 sellx.append(vwap.index[i])
                 selly.append(vwap['Average'][i])
 
-        elif (vwap['Average'][i] - vwap['vwap'][i] < 0.1): #buy
+        elif (vwap['Average'][i] - vwap['vwap'][i] < 2): #buy
             boolean = b.buy(2, float(vwap['Average'][i]), i)
 
             if boolean:
