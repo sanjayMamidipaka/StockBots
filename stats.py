74511727588101ca2ba48de973f643dad4a6666b
Sanjay Mamidipaka <sanjay.mamidipaka@gmail.com>
2020-05-07 16:21:04 -0400

    testing

--- stats.py
+++ stats.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY'
-r = '{}&symbol={}&interval={}&apikey={}&datatype={}&outputsize={}'.format(url, 'fb', '1min','9YM6MWUWHMZN05MB', 'csv','full')
+r = '{}&symbol={}&interval={}&apikey={}&datatype={}&outputsize={}'.format(url, 'msft', '1min','9YM6MWUWHMZN05MB', 'csv','full')
 
 initial = pd.read_csv(r, index_col='timestamp', parse_dates=True)
 initial = initial.iloc[::-1]
@@ -14,7 +14,7 @@ stock = StockDataFrame.retype(initial)
 initial['bband1'] = stock['boll_lb']
 initial['bband2'] = stock['boll_ub']
 initial['rsi'] = stock['rsi_6']
-initialInvestment = 10000.0
+initialInvestment = 100000.0
 initial.dropna()
 b = backtester.Backtester(initialInvestment, initial)
 sellx = []
@@ -24,7 +24,7 @@ buyy = []
 
 for i in range(0,len(initial.index)):
     if (stock['rsi_6'][i] < 30): #buy
-        if b.buy(1, initial['close'][i], i):
+        if b.buy(150, initial['close'][i], i):
             buyx.append(initial.index[i])
             buyy.append(initial['close'][i])
 
